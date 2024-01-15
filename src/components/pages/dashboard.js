5417c1ec825a5b91126365a214e7e82caa28d672
Leart23 <leartmusahu@gmail.com>
2024-01-15 01:33:14 +0100

    little changes

--- src/components/pages/dashboard.js
+++ src/components/pages/dashboard.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import EditUserModal from "./EditUserModal";
 
+
+
 const Dashboard = () => {
   const [users, setUsers] = useState([]);
   const [selectedUser, setSelectedUser] = useState(null);
@@ -16,6 +18,7 @@ const Dashboard = () => {
     Password: "",
     Role: "",
   });
+ ;
 
   useEffect(() => {
     const fetchUsers = async () => {
@@ -26,8 +29,8 @@ const Dashboard = () => {
       } catch (error) {
         console.error("Error fetching user data:", error);
       }
+      
     };
-
     fetchUsers();
   }, [showDeleteModal]);
 
@@ -148,14 +151,16 @@ const Dashboard = () => {
             </svg>
             Home
             </button>
+            <div className="border-b border-gray-600"></div>
           </li>
           <li className="">
             <button className="hover:text-gray-300 flex flex-row mt-2" onClick={handleAddUserModalOpen}>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" class="w-8 h-6 pr-2 ">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-8 h-6 pr-2 ">
             <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
             </svg>
               Add a New User
             </button>
+            <div className="border-b border-gray-600"></div>
           </li>
         </ul>
       </div>
@@ -175,7 +180,7 @@ const Dashboard = () => {
             </div>
           </div>
 
-          <div className="w-full h-96">
+          <div className="w-[90%] h-96">
             <table className="w-[90%] bg-stone-50 rounded-lg mt-8 ml-10">
               <thead>
                 <tr>
@@ -199,9 +204,10 @@ const Dashboard = () => {
                         <div className="flex m-2">
                           <button
                             onClick={() => handleEditClick(user)}
-                            className="bg-gray-800 text-white px-4 py-2 rounded cursor-pointer"
                           >
-                            Edit
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-10 h-7 mt-2">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+                            </svg>                           
                           </button>
                           <svg
                             onClick={() => {
@@ -213,7 +219,7 @@ const Dashboard = () => {
                             viewBox="0 0 24 24"
                             strokeWidth="1.5"
                             stroke="currentColor"
-                            className="w-6 h-6 ml-4 mt-2 cursor-pointer"
+                            className="w-10 h-7 ml-4 mt-2 cursor-pointer"
                           >
                             <path
                               strokeLinecap="round"
